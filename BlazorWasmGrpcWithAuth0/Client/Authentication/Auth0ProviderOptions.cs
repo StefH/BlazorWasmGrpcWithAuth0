8cdf9ef95441ddf66bb61ef8d37b72c26e3a07e8
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-07-03 14:28:52 +0000

    Audience

--- BlazorWasmGrpcWithAuth0/Client/Authentication/Auth0ProviderOptions.cs
+++ BlazorWasmGrpcWithAuth0/Client/Authentication/Auth0ProviderOptions.cs
@@ -1,14 +1,24 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.AspNetCore.Components.WebAssembly.Authentication
 {
     public class Auth0ProviderOptions : OidcProviderOptions
     {
+        private const string AudienceKey = "audience";
+
         /// <summary>
-        /// Gets or sets the audience of the application.
+        /// Sets the audience of the application.
         /// </summary>
-        [JsonPropertyName("audience")]
         public string Audience { get; set; }
+
+        /// <summary>
+        /// Extra Query Parameters
+        /// 
+        /// See https://github.com/IdentityModel/oidc-client-js/blob/dev/src/OidcClient.js#L65
+        /// </summary>
+        [JsonPropertyName("extraQueryParams")]
+        public IDictionary<string, string> ExtraQueryParams => new Dictionary<string, string> { { AudienceKey, Audience } };
     }
 }
